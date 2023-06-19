bce4876478c878540df100e4a820b8d3981e35df
Ziyuan Lin <zl4140@nyu.edu>
2023-06-19 15:21:38 -0400

    add handpose model

---
+++ src/Handpose/index.js
@@ -0,0 +1,99 @@
+// Copyright (c) 2020 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/*
+ * Handpose: Palm detector and hand-skeleton finger tracking in the browser
+ * Ported and integrated from all the hard work by: https://github.com/tensorflow/tfjs-models/tree/master/handpose
+ */
+import * as tf from "@tensorflow/tfjs";
+import model from "@mediapipe/hands";
+import * as handPoseDetection from "@tensorflow-models/hand-pose-detection";
+import { EventEmitter } from "events";
+import callCallback from "../utils/callcallback";
+import handleArguments from "../utils/handleArguments";
+import { mediaReady } from "../utils/imageUtilities";
+
+class Handpose extends EventEmitter {
+  /**
+   * Create Handpose.
+   * @param {HTMLVideoElement} [video] - An HTMLVideoElement.
+   * @param {object} [options] - An object with options.
+   * @param {function} [callback] - A callback to be called when the model is ready.
+   */
+  constructor(video, options, callback) {
+    super();
+
+    this.video = video;
+    /**
+     * @type {null|handposeCore.HandPose}
+     */
+    this.model = null;
+    this.modelReady = false;
+    this.config = options;
+
+    this.ready = callCallback(this.loadModel(), callback);
+  }
+
+  /**
+   * Load the model and set it to this.model
+   * @return {this} the Handpose model.
+   */
+  async loadModel() {
+    const mediaPipeHands = handPoseDetection.SupportedModels.MediaPipeHands;
+    const modelConfig = {
+      ...this.config,
+      runtime: "mediapipe",
+      solutionPath: "../node_modules/@mediapipe/hands",
+    };
+
+    this.model = await handPoseDetection.createDetector(
+      mediaPipeHands,
+      modelConfig
+    );
+    this.modelReady = true;
+
+    if (this.video) {
+      this.predict();
+    }
+
+    return this;
+  }
+
+  /**
+   * @return {Promise<handposeCore.AnnotatedPrediction[]>} an array of predictions.
+   */
+  async predict(inputOr, cb) {
+    const { image, callback } = handleArguments(this.video, inputOr, cb);
+    if (!image) {
+      throw new Error("No input image found.");
+    }
+    await mediaReady(image, false);
+    const { flipHorizontal } = this.config;
+    const predictions = await this.model.estimateHands(image, flipHorizontal);
+    const result = predictions;
+    // Soon, we will remove the 'predict' event and prefer the 'hand' event. During
+    // the interim period, we will both events.
+    this.emit("predict", result);
+    this.emit("hand", result);
+
+    if (this.video) {
+      return tf.nextFrame().then(() => this.predict());
+    }
+
+    if (typeof callback === "function") {
+      callback(result);
+    }
+
+    return result;
+  }
+}
+
+const handpose = (...inputs) => {
+  const { video, options = {}, callback } = handleArguments(...inputs);
+  const instance = new Handpose(video, options, callback);
+  return callback ? instance : instance.ready;
+};
+
+export default handpose;
