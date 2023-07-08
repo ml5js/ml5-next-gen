f2e45443b85c502f03b923ff28e583dd24c02ea2
VivianChenyc5519 <yc5519@nyu.edu>
2023-07-08 20:42:04 +0800

    add new files to utils

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import poseDetection from "./PoseDetection";
 import * as tf from "@tensorflow/tfjs";
 import * as tfvis from "@tensorflow/tfjs-vis";
 import p5Utils from "./utils/p5Utils";
+import preloadRegister from "./utils/p5PreloadHelper";
 
+const withPreload = {
+  bodyPix,
+}
 export default Object.assign(
   { p5Utils },
+  preloadRegister(withPreload),
   {
     tf,
     tfvis,
     neuralNetwork,
     handpose,
     poseDetection,
+    bodyPix,
   }
 );
