3f0053d4a4f9dfec6a50343c5b473f5fbf0f89a0
Ziyuan Lin <zl4140@nyu.edu>
2023-07-14 16:55:21 -0400

    add jest test scripts

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  globalSetup: "./setupTests.js",
+  passWithNoTests: true,
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    resources: "usable", // Load image resources
+  },
+};
+
+module.exports = config;
---
+++ setupTests.js
@@ -0,0 +1,15 @@
+const { ImageData } = require("canvas");
+// require('@tensorflow/tfjs-node');
+
+async function setupTests() {
+  console.log("Beginning setup");
+
+  // Use the node-canvas ImageData polyfill
+  if (!global.ImageData) {
+    global.ImageData = ImageData;
+  }
+
+  console.log("Setup complete");
+}
+
+module.exports = setupTests;
