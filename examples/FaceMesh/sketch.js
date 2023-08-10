254182e6b4a876e932dd094d8f7b38c713f93485
B2xx <113410244+B2xx@users.noreply.github.com>
2023-08-10 15:53:48 +0800

    Add function to show the seriel number of the points nearest the position of moouse and add a function to store the basic data of different face features

--- examples/FaceMesh/sketch.js
+++ examples/FaceMesh/sketch.js
@@ -47,8 +47,14 @@ function draw() {
   //We call function to draw certain facial features
   //We have faceOval,rightEyebrow, leftEyebrow, rightEye, leftEye, lips
   drawFeatures();
-  //Interactive Elements
-  mask();
+  //We call function to store basic data for certain facial features
+  //We have faceOval,rightEyebrow, leftEyebrow, rightEye, leftEye, lips
+  featuresData();
+  // //Show the index of the points
+  // directPoints();
+
+  // //Interactive Elements
+  // mask();
 }
 
 // A function to draw bounding boxes
@@ -112,22 +118,83 @@ function drawFeatures(){
       for (let j = 0; j < face.keypoints.length; j += 1) {
         // console.log(Object.values(keypoint)[3]); //The name of all facial features
         const keypoint = face.keypoints[j];
-        
+
+        //draw keypoints for lips
         if (Object.values(keypoint)[3]=="lips") {
           fill(255, 255, 0);
-          //draw keypoints
           noStroke();
           ellipse(keypoint.x, keypoint.y, 5, 5);
-          
         };
-      }
-    }
+      };
+    };
   }      
 }
 
-//Find the points
-function nearestPoints(){
+//A function to store basic data for certain facial features, this example is for lips
+//We have faceOval,rightEyebrow, leftEyebrow, rightEye, leftEye, lips
+function featuresData(){
+  if (predictions.length > 0){
+    for (let i = 0; i < predictions.length; i += 1) {
+      const face = predictions[i];
+      const fKeypointX = [];
+      const fKeypointY = [];
+      for (let j = 0; j < face.keypoints.length; j += 1) {
+        // console.log(Object.values(keypoint)[3]); //The name of all facial features
+        const keypoint = face.keypoints[j];
+        
+        if (Object.values(keypoint)[3]=="lips") {
+          fKeypointX.push(keypoint.x);
+          fKeypointY.push(keypoint.y);
+        };
+      };
+      //Create an example class of important data of facial features
+          const featuresData = {
+            lips: {
+              centerX: avg(fKeypointX),
+              centerY: avg(fKeypointY),
+              fWidth: length(fKeypointX),
+              fheight: length(fKeypointY),
+            }
+          };
+          // console.log(featuresData);
+    };
+    function avg(x){
+      return (max(x)+min(x))/2
+          }
+    function length(x){
+      return max(x)-min(x)
+          }
+  }      
+}
 
+//Show the index of the points
+function directPoints(){
+  let dMouse = [];
+  let closest = 0;
+
+  if (predictions.length > 0){
+    for (let i = 0; i < predictions.length; i += 1) {
+      const face = predictions[i];
+      for (let j = 0; j < face.keypoints.length; j += 1) {
+          const keypoint = face.keypoints[j];
+
+          //calculate the distance between mouse and points
+          let d = dist(keypoint.x,keypoint.y,mouseX,mouseY);
+          
+          dMouse.push(d);
+        }
+
+      let minimum = min(dMouse);
+      closest = dMouse.indexOf(minimum);
+
+      fill(255,0,0);
+      ellipse(predictions[i].keypoints[closest].x, predictions[i].keypoints[closest].y, 5, 5);
+
+      console.log(closest);
+
+      dMouse.splice(0,dMouse.length);
+    }
+  }
 }
 
 //Interactive Elements
