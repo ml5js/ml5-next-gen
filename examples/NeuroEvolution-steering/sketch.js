4097cdf57e3dec321deb2ac9bc50e0946642372b
Daniel Shiffman <daniel.shiffman@gmail.com>
2023-08-14 17:58:35 -0400

    simplifying steering example

--- examples/NeuroEvolution-steering/sketch.js
+++ examples/NeuroEvolution-steering/sketch.js
@@ -1,27 +1,22 @@
 let vehicles = [];
 let timeSlider;
 let lifetime = 0;
-
 let target;
 
 function setup() {
   createCanvas(640, 240);
-  // Initialize a set number of vehicles
+  ml5.tf.setBackend("cpu");
   for (let i = 0; i < 50; i++) {
     vehicles[i] = new Vehicle(random(width), random(height));
   }
-  ml5.tf.setBackend("cpu");
-
   target = createVector(random(width), random(height));
   timeSlider = createSlider(1, 20, 1);
 }
 
 function draw() {
   background(255);
-
   for (let i = 0; i < timeSlider.value(); i++) {
     for (let v of vehicles) {
-      v.edges();
       v.think(target);
       v.eat(target);
       v.update();
@@ -29,28 +24,13 @@ function draw() {
     lifetime++;
   }
 
-  let best = null;
-  let record = -1;
   for (let v of vehicles) {
     v.show();
-    if (v.fitness > record) {
-      record = v.fitness;
-      best = v;
-    }
   }
 
-  best.show();
+  fill(0, 0, 255, 150);
   noStroke();
-  fill(255, 0, 0, 100);
-  circle(best.position.x, best.position.y, 64);
-  fill(0);
-  text(best.fitness, best.position.x + 12, best.position.y);
-  fill(0, 0, 255, 100);
   circle(target.x, target.y, 32);
-
-  stroke(0, 100);
-  strokeWeight(1);
-  //line(target.x, target.y, best.position.x, best.position.y);
 
   if (lifetime > 500) {
     normalizeFitness();
@@ -58,32 +38,14 @@ function draw() {
     target = createVector(random(width), random(height));
     lifetime = 0;
   }
-  // if (allVehiclesDead()) {
-  // }
-
   fill(0);
   text(lifetime, 10, 30);
 }
 
-// function allVehiclesDead() {
-//   for (let v of vehicles) {
-//     if (v.alive) {
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-
 function normalizeFitness() {
-  let recordFitness = 0;
   for (let v of vehicles) {
-    if (v.fitness > recordFitness) {
-      recordFitness = v.fitness;
-    }
     v.fitness = pow(2, v.fitness);
   }
-  console.log(recordFitness);
-
   let sum = 0;
   for (let v of vehicles) {
     sum += v.fitness;
--- examples/NeuroEvolution-steering/vehicle.js
+++ examples/NeuroEvolution-steering/vehicle.js
@@ -23,7 +23,6 @@ class Vehicle {
     let d = p5.Vector.dist(this.position, food);
     if (d < 10) {
       this.fitness++;
-      // this.position = createVector(random(width), random(height));
       target = createVector(random(width), random(height));
     }
   }
@@ -35,10 +34,6 @@ class Vehicle {
 
     // Predicting the force to apply
     const outputs = this.brain.predictSync(inputs);
-    // let x = 2 * outputs[0].value - 1;
-    // let y = 2 * outputs[1].value - 1;
-    // let force = createVector(x, y); //.setMag(magnitude);
-    // force.setMag(1);
     let angle = outputs[0].value * TWO_PI;
     let magnitude = outputs[1].value;
     let force = p5.Vector.fromAngle(angle).setMag(magnitude);
@@ -54,30 +49,11 @@ class Vehicle {
     this.position.add(this.velocity);
     // Reset acceleration to 0 each cycle
     this.acceleration.mult(0);
-
-    // if (
-    //   this.position.x > width ||
-    //   this.position.y > height ||
-    //   this.position.x < 0 ||
-    //   this.position.y < 0
-    // ) {
-    //   this.alive = false;
-    // }
   }
 
   applyForce(force) {
     // We could add mass here if we want A = F / M
     this.acceleration.add(force);
-  }
-
-  // Wraparound
-  edges() {
-    // this.position.x = constrain(this.position.x, 0, width);
-    // this.position.y = constrain(this.position.y, 0, height);
-    //if (this.position.x < -this.r) this.position.x = width + this.r;
-    //if (this.position.y < -this.r) this.position.y = height + this.r;
-    //if (this.position.x > width + this.r) this.position.x = -this.r;
-    //if (this.position.y > height + this.r) this.position.y = -this.r;
   }
 
   show() {
@@ -94,18 +70,5 @@ class Vehicle {
     vertex(-this.r * 2, this.r);
     endShape(CLOSE);
     pop();
-
-    //fill(0);
-    //noStroke();
-    //text(this.fitness, this.position.x + 12, this.position.y);
-
-    // let d = p5.Vector.dist(this.position, this.target);
-    // if (d < 50) {
-    // stroke(0, 200);
-    // strokeWeight(1);
-    // line(this.position.x, this.position.y, this.target.x, this.target.y);
-    // fill(0, 255, 0, 100);
-    // circle(this.target.x, this.target.y, 8);
-    //}
   }
 }
