f82d771e9bfbe92998a16d0d486fdf2b7d1cbd37
gohai <gottfried.haider@gmail.com>
2023-09-16 16:52:12 +0800

    Sentiment: Add some cleanups
    Update p5 to 1.6
    Add newlines at end of files
    Don't use const in examples
    Don't use template literals in example
    Don't use unneeded === in example
    Run prettifier

--- examples/Sentiment/sketch.js
+++ examples/Sentiment/sketch.js
@@ -5,40 +5,40 @@ let inputBox;
 let sentimentResult;
 
 function setup() {
-    noCanvas();
-    // initialize sentiment analysis model
-    sentiment = ml5.sentiment('movieReviews', modelReady);
+  noCanvas();
+  // initialize sentiment analysis model
+  sentiment = ml5.sentiment("movieReviews", modelReady);
 
-    // setup the html environment
-    statusEl = createP('Loading Model...');
-    inputBox = createInput('Today is the happiest day and is full of rainbows!');
-    inputBox.attribute('size', '75');
-    submitBtn = createButton('submit');
-    sentimentResult = createP('sentiment score:');
+  // setup the html environment
+  statusEl = createP("Loading Model...");
+  inputBox = createInput("Today is the happiest day and is full of rainbows!");
+  inputBox.attribute("size", "75");
+  submitBtn = createButton("submit");
+  sentimentResult = createP("Sentiment score:");
 
-    // predicting the sentiment on mousePressed()
-    submitBtn.mousePressed(getSentiment);
+  // predicting the sentiment on mousePressed()
+  submitBtn.mousePressed(getSentiment);
 }
 
 function getSentiment() {
-    // get the values from the input
-    const text = inputBox.value();
+  // get the values from the input
+  let text = inputBox.value();
 
-    // make the prediction
-    const prediction = sentiment.predict(text);
+  // make the prediction
+  let prediction = sentiment.predict(text);
 
-    // display sentiment result on html page
-    sentimentResult.html(`Sentiment score: ${prediction.score}`);
+  // display sentiment result on html page
+  sentimentResult.html("Sentiment score: " + prediction.score);
 }
 
 function modelReady() {
-    // model is ready
-    statusEl.html('model loaded');
+  // model is ready
+  statusEl.html("Model loaded");
 }
 
 // predicting the sentiment when 'Enter' key is pressed
 function keyPressed() {
-    if (keyCode === ENTER) {
-        getSentiment();
-    }
+  if (keyCode == ENTER) {
+    getSentiment();
+  }
 }
