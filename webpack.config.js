79b4a47553f58980ea455f54f443affc491f899d
ziyuan-linn <zl4140@nyu.edu>
2023-09-24 17:44:46 -0400

    update webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,66 @@
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = function (env, argv) {
-  return {
-    context: __dirname,
-    entry: "./src/index.js",
+const commonConfig = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    filename: "ml5.js",
+    path: resolve(__dirname, "dist"),
+    library: {
+      name: "ml5",
+      type: "umd",
+      export: "default",
+    },
+  },
+};
 
-    mode: env.production ? "production" : "development",
-    devtool: env.production ? "source-map" : "inline-source-map",
-    output: {
-      filename: "ml5.js",
-      path: resolve(__dirname, "dist"),
-      publicPath: "/dist/",
-      library: {
-        name: "ml5",
-        type: "umd",
-        export: "default",
+const developmentConfig = {
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    publicPath: "/dist/",
+  },
+  devServer: {
+    port: 8080,
+    allowedHosts: "all",
+    static: [
+      {
+        directory: resolve(__dirname, "dist"),
+        publicPath: "/dist",
+        watch: true,
       },
-    },
-    devServer: {
-      port: 8080,
-      allowedHosts: "all",
-      static: [
-        {
-          directory: resolve(__dirname, "dist"),
-          publicPath: "/dist",
-          watch: true,
-        },
-        {
-          directory: resolve(__dirname, "examples"),
-          publicPath: "/examples",
-          watch: true,
-        },
-      ],
-      open: "/examples",
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "ml5",
-      }),
+      {
+        directory: resolve(__dirname, "examples"),
+        publicPath: "/examples",
+        watch: true,
+      },
     ],
-  };
+    open: "/examples",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ml5",
+    }),
+  ],
+};
+
+const productionConfig = {
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    publicPath: "/",
+  },
+};
+
+module.exports = function (env, args) {
+  switch (args.mode) {
+    case "development":
+      return merge(commonConfig, developmentConfig);
+    case "production":
+      return merge(commonConfig, productionConfig);
+    default:
+      throw new Error("No matching configuration was found!");
+  }
 };
