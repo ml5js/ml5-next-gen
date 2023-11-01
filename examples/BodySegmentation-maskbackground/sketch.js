f6d30060f5aa53bc2c3fa4015d18c30b33e0be7b
Ziyuan Lin <zl4140@nyu.edu>
2023-11-01 14:42:11 -0400

    update example sketches

--- examples/BodySegmentation-maskbackground/sketch.js
+++ examples/BodySegmentation-maskbackground/sketch.js
@@ -8,41 +8,38 @@ ml5 Example
 BodyPix
 === */
 
-let bodypix;
+let bodyPix;
 let video;
 let segmentation;
 
 let options = {
-  outputStride: 16, //adjust the output stride and see which one works best!
-  multiSegmentation: false,
-  segmentBodyParts: true,
-  flipHorizontal: true,
+  maskType: "background",
 };
 
+function preload() {
+  bodyPix = ml5.bodyPix(video, options);
+}
+
 function setup() {
-  createCanvas(480, 360);
+  createCanvas(640, 480);
   // Create the video
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
-  bodypix = ml5.bodyPix(video, options, modelReady);
-  bodypix.on("bodypix", gotResults);
-}
 
-// Event for body segmentation
-function gotResults(result) {
-  // Save the latest part mask from the model in global variable "segmentation"
-  segmentation = result;
-  //Draw the video
-  image(video, 0, 0, width, height);
-  image(segmentation.backgroundMask, 0, 0, width, height);
-  tint(255, 128); // opacity tuning
-}
-
-// Event when model is loaded
-function modelReady() {
-  console.log("Model ready!");
+  bodyPix.detectStart(video, gotResults);
 }
 
 function draw() {
+  background(255);
+  image(video, 0, 0);
+  if (segmentation) {
+    tint(255, 200);
+    image(segmentation, 0, 0, width, height);
+    tint(255, 255);
+  }
+}
+// callback function for body segmentation
+function gotResults(result) {
+  segmentation = result.mask;
 }
