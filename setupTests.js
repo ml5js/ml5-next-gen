b3d1057eb46714d3c7ef2345ae0864c9267ff055
Linda Paiste <lindapaiste@gmail.com>
2024-03-16 12:57:57 -0500

    Set up testing (#82)
    * add jest test scripts
    
    * setup jest
    
    * Passing tests for BodyPix
    
    ---------
    
    Co-authored-by: Ziyuan Lin <zl4140@nyu.edu>

---
+++ setupTests.js
@@ -0,0 +1,27 @@
+const { ImageData } = require("canvas");
+import '@tensorflow/tfjs-node'; // loads the tensorflow/node backend to the registry
+import crossFetch from 'cross-fetch';
+import * as tf from '@tensorflow/tfjs';
+
+async function setupTests() {
+
+  console.log("Beginning setup");
+
+  await tf.setBackend('tensorflow');
+  tf.env().set('IS_BROWSER', false);
+  tf.env().set('IS_NODE', true);
+
+  // Use the node-canvas ImageData polyfill
+  if (!global.ImageData) {
+    global.ImageData = ImageData;
+  }
+
+  // Use cross-fetch as a polyfill for the browser fetch
+  if (!global.fetch) {
+    global.fetch = crossFetch;
+  }
+
+  console.log("Setup complete");
+}
+
+module.exports = setupTests;
