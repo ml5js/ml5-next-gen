6c0f4e5774f775429637e0a5272bbe42a4a9a4ad
ziyuan-linn <zl4140@nyu.edu>
2024-03-21 21:15:46 -0400

    add save and load examples

---
+++ examples/NeuralNetwork-load/sketch.js
@@ -0,0 +1,100 @@
+// Copyright (c) 2024 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// This example will load a pre-trained model that recognizes hand gestures for rock paper scissors.
+
+let classifier;
+let handPose;
+let video;
+let hands = [];
+let classification = "";
+let isModelLoaded = false;
+
+function preload() {
+  // Load the handPose model
+  handPose = ml5.handPose();
+}
+
+function setup() {
+  createCanvas(640, 480);
+
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  // For this example to work across all browsers
+  // "webgl" or "cpu" needs to be set as the backend
+  ml5.setBackend("webgl");
+
+  // Set up the neural network
+  let classifierOptions = {
+    task: "classification",
+  };
+  classifier = ml5.neuralNetwork(classifierOptions);
+
+  const modelDetails = {
+    model: "model/model.json",
+    metadata: "model/model_meta.json",
+    weights: "model/model.weights.bin",
+  };
+
+  classifier.load(modelDetails, modelLoaded);
+
+  // Start the handPose detection
+  handPose.detectStart(video, gotHands);
+}
+
+function draw() {
+  //Display the webcam video
+  image(video, 0, 0, width, height);
+
+  // Draw the handPose keypoints
+  if (hands[0]) {
+    let hand = hands[0];
+    for (let i = 0; i < hand.keypoints.length; i++) {
+      let keypoint = hand.keypoints[i];
+      fill(0, 255, 0);
+      noStroke();
+      circle(keypoint.x, keypoint.y, 10);
+    }
+  }
+
+  // If the model is loaded, make a classification and display the result
+  if (isModelLoaded && hands[0]) {
+    let inputData = flattenHandData();
+    classifier.classify(inputData, gotClassification);
+    textSize(64);
+    fill(0, 255, 0);
+    text(classification, 20, 60);
+  }
+}
+
+// convert the handPose data to a 1D array
+function flattenHandData() {
+  let hand = hands[0];
+  let handData = [];
+  for (let i = 0; i < hand.keypoints.length; i++) {
+    let keypoint = hand.keypoints[i];
+    handData.push(keypoint.x);
+    handData.push(keypoint.y);
+  }
+  return handData;
+}
+
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  hands = results;
+}
+
+// Callback function for when the classifier makes a classification
+function gotClassification(results) {
+  classification = results[0].label;
+}
+
+// Callback function for when the pre-trained model is loaded
+function modelLoaded() {
+  isModelLoaded = true;
+}
