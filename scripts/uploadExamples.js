c7442d4f579581d76ec82c51942c68df38df02af
ziyuan-linn <zl4140@nyu.edu>
2024-05-25 16:13:25 -0400

    refactor and add delete function

--- scripts/uploadExamples.js
+++ scripts/uploadExamples.js
@@ -2,10 +2,16 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 const objectID = require("bson-objectid");
 const ml5Version = require("../package.json").version;
-let sessionID = "";
+
 dotenv.config();
 
 const P5_URL = "https://editor.p5js.org";
+const TEXT_FILE_REGEX =
+  /.+\.(json|txt|csv|tsv|vert|frag|js|css|html|htm|jsx|xml|stl|mtl)$/i;
+
+let sessionID = "";
+const manualUploadList = [];
+let manualUploadFlag = false;
 
 /**
  * Get the session ID for the p5 web editor.
@@ -56,13 +62,24 @@ async function uploadSketch(sketch) {
     },
     body: JSON.stringify(sketch),
   });
-  const data = await res.json();
+  if (res.status != 200) {
+    console.log("🔴Failed to upload sketch: " + sketch.name);
+  } else {
+    if (manualUploadFlag) {
+      console.log("🟡Manual upload of non-text file required: " + sketch.name);
+      manualUploadFlag = false;
+      console.log(manualUploadList);
+    }
+    console.log("🟢Successfully uploaded sketch: " + sketch.name);
+  }
 }
 
 /**
  * Upload a binary file to the p5 web editor.
  * @param {string} filePath - The file path.
  * @returns
+ *
+ * @todo: This function does not work, non-text files need to be uploaded manually for now
  */
 async function uploadFile(filePath) {
   const file = fs.readFileSync(filePath);
@@ -81,18 +98,60 @@ async function uploadFile(filePath) {
   //return data;
 }
 
+getExistingFiles = async () => {
+  const res = await fetch(
+    P5_URL + `/editor/${process.env.P5_USERNAME}/projects`,
+    {
+      method: "GET",
+      headers: {
+        Cookie: `sessionId=${sessionID}`,
+      },
+    }
+  );
+  if (!(res.status == 200 || res.status == 304)) {
+    throw new Error(
+      "Failed to get existing files. p5 Server returned status code: " +
+        res.status
+    );
+  }
+  return await res.json();
+};
+
+deleteFile = async (file) => {
+  const res = await fetch(P5_URL + `/editor/projects/${file.id}`, {
+    method: "DELETE",
+    headers: {
+      Cookie: `sessionId=${sessionID}`,
+    },
+  });
+  if (res.status != 200) {
+    console.log("🔴Failed to delete file: " + file.name);
+  } else {
+    console.log("🟢Successfully deleted file: " + file.name);
+  }
+};
+
 /**
  * Get the file path of each item in a given directory.
  *
  * @param {string} dir - The directory path.
  * @returns - An array of file paths.
  */
-function getFilepathList(dir) {
+function getFilepaths(dir) {
   let files = [];
   fs.readdirSync(dir).forEach((file) => {
     files.push(`${dir}/${file}`);
   });
   return files;
+}
+
+/**
+ * Check if a file is a text file.
+ * @param {string} filename
+ * @returns {boolean} True if the file is a text file, false otherwise.
+ */
+function isTextFile(filename) {
+  return TEXT_FILE_REGEX.test(filename);
 }
 
 /**
@@ -103,16 +162,19 @@ function getFilepathList(dir) {
  * @returns {Object} The file object.
  */
 function createFileObject(fileID, filePath) {
-  const fileName = filePath.split("/").pop();
-  const fileExtension = fileName.split(".").pop();
+  const filename = filePath.split("/").pop();
   let fileContent = "";
-  if (fileExtension === "bin") {
-  } else {
+  if (isTextFile(filename)) {
     fileContent = fs.readFileSync(filePath, "utf8");
+    fileContent = replaceWithCdnURL(fileContent);
+  } else {
+    //TODO: handle upload non-text files
+    manualUploadList.push(filePath);
+    manualUploadFlag = true;
+    return null;
   }
-  fileContent = replaceWithCdnURL(fileContent);
   return {
-    name: fileName,
+    name: filename,
     id: fileID,
     _id: fileID,
     fileType: "file",
@@ -151,23 +213,27 @@ function replaceWithCdnURL(content) {
 }
 
 /**
- * Recursively add files to the sketch object.
- * @param {*} folderPath - The folder path.
- * @param {*} fileArray - The array storing file objects in the sketch.
- * @param {*} childrenArray - The array storing the children of the folder.
+ * Recursively add files and folders to the sketch object.
+ * @param {*} currentPath - The current directory path
+ * @param {*} fileArray - The array storing file and folder objects in the sketch.
+ * @param {*} childrenArray - The array storing the children ids of the folder.
  */
-async function recursiveAddFiles(folderPath, fileArray, childrenArray) {
-  const files = getFilepathList(folderPath);
-  files.forEach((filePath) => {
+async function recursiveAddFiles(currentPath, fileArray, childrenArray) {
+  const filepaths = getFilepaths(currentPath);
+  filepaths.forEach((filePath) => {
     const fileID = objectID().toString();
     if (fs.lstatSync(filePath).isDirectory()) {
       const folder = createFolderObject(fileID, filePath);
       fileArray.push(folder);
+      childrenArray.push(fileID);
       recursiveAddFiles(filePath, fileArray, folder.children);
     } else {
-      fileArray.push(createFileObject(fileID, filePath));
+      const fileObject = createFileObject(fileID, filePath);
+      if (fileObject) {
+        fileArray.push(fileObject);
+        childrenArray.push(fileID);
+      }
     }
-    childrenArray.push(fileID);
   });
 }
 
@@ -186,19 +252,33 @@ function createSketchObject(sketchDirPath) {
 
   recursiveAddFiles(sketchDirPath, sketch.files, sketch.files[0].children);
 
+  // Set the sketch.js file as the default selected file.
+  for (const file of sketch.files) {
+    if (file.name == "sketch.js") {
+      file.isSelectedFile = true;
+      break;
+    }
+  }
+
   return sketch;
 }
 
 /**
- * This script uploads the examples to the server.
+ * Entry point of the script.
+ * This script uploads each sketch in the examples to the server.
  */
 async function main() {
   sessionID = await getP5SessionID();
-  const sketchPaths = getFilepathList("examples");
+  const sketchPaths = getFilepaths("examples");
+  const oldFiles = await getExistingFiles();
+  for (const file of oldFiles) {
+    await deleteFile(file);
+  }
 
-  const sketchObject = createSketchObject(sketchPaths[17]);
-  console.log(sketchObject);
-  await uploadSketch(sketchObject);
+  for (const sketchPath of sketchPaths) {
+    const sketchObject = createSketchObject(sketchPath);
+    await uploadSketch(sketchObject);
+  }
 }
 
 main();
