b8685d23a9f311cb54bf1161858f7250a6c6e59d
mop9047 <mop9047@nyu.edu>
2024-07-05 23:49:05 +0800

    Created methods based on circle or square mousexy

--- src/LSTM/index.js
+++ src/LSTM/index.js
@@ -1,5 +1,8 @@
 
 import * as tf from "@tensorflow/tfjs";
+import * as tfvis from "@tensorflow/tfjs-vis";
+import nnUtils from "../NeuralNetwork/NeuralNetworkUtils";
+
 // import '@tensorflow/tfjs-node';
 // import callCallback from "../utils/callcallback";
 
@@ -12,14 +15,28 @@ class LSTMify{
   }
 
   createArchitecture() {
+
+    // Create the model
+    this.model = tf.sequential();
+
+    // Add the LSTM layers with the initializer
     this.model.add(tf.layers.lstm({
-      units: 40,
-      inputShape: [20,2]
+      units: 50,
+      inputShape: [20, 2],
+      activation: 'relu',
+      returnSequences: true,
+      kernelInitializer: tf.initializers.glorotNormal(),
+      recurrentInitializer: tf.initializers.glorotNormal(),
+      biasInitializer: tf.initializers.glorotNormal(),
+
     }));
 
-    this.model.add(tf.layers.dense({
-      units: 20,
-      activation: 'relu',
+
+    this.model.add(tf.layers.lstm({
+      units: 50,
+      kernelInitializer: tf.initializers.glorotNormal(),
+      recurrentInitializer: tf.initializers.glorotNormal(),
+      biasInitializer: tf.initializers.glorotNormal(),
     }));
 
     this.model.add(tf.layers.dense({
@@ -29,27 +46,90 @@ class LSTMify{
   }
 
   compileModel(){
+    const optimizer = tf.train.adam(0.002)
+    // const optimizer = tf.train.adadelta(0.05)
+
     this.model.compile({
-      optimizer: 'adam',
-      loss: 'categoricalCrossentropy',
+      optimizer: optimizer,
+      loss: 'binaryCrossentropy',
       metrics: ['accuracy']
     });
-  }
-
-  fitModel(xs,ys){
-    this.model.fit(xs, ys,{
-      epochs: 20,
-      batchSize: 32,
-      callbacks: {
-        onEpochEnd: (epoch, logs) => {
-        console.log(`Epoch ${epoch + 1}: loss = ${logs.loss}, accuracy = ${logs.acc}`);
-      }}
-    })
   }
   
   summarizeModel(){
     this.model.summary()
   }
+
+  toTensors(x,y){
+    const x_tensor = tf.tensor(x);
+    const y_tensor = tf.tensor(y);
+
+    return [x_tensor,y_tensor]
+  }
+
+  async fitModel(xs,ys){
+    this.loggers = []
+    this.history = await this.model.fit(xs, ys,{ 
+      epochs: 50,
+      batchSize: 16,
+      callbacks: {
+        onEpochEnd: (epoch, logs) => {
+        this.loggers.push(logs)
+        console.log(`Epoch ${epoch + 1}: loss = ${logs.loss}, accuracy = ${logs.acc}`);
+      }
+      // callbacks: {
+      //   onEpochEnd: async (epoch, logs) => {
+      //     // Display the loss and accuracy at the end of each epoch
+      //     this.loggers.push(logs)
+      
+      //     // Plot loss and accuracy
+      //     tfvis.show.history(
+      //       { name: 'Training Performance' },
+      //       this.loggers,
+      //       ['loss', 'accuracy']  // or ['loss', 'acc'] based on your metrics
+      //     );
+      //   },
+      // }
+    }})
+  }
+
+  modelSummary() {
+    console.log(this.history);
+    tfvis.show.history({ name: 'Training Performance' }, this.loggers, ['loss', 'accuracy']);
+  }
+
+  // async predict(data){
+  //   const predictions =  this.model.predict(data)
+  //   const predict = await predictions.array();
+  //   console.log(typeof predict)
+  //   predict.array().then(array => {
+  //     console.log(array);
+  //     // return array
+  //   })
+  //   // console.log("this is the one")
+  //   // return array_ver
+
+  // }
+
+  predict(_inputs) {
+    const output = tf.tidy(() => {
+      return this.model.predict(_inputs);
+    });
+    const result = output.arraySync();
+
+    output.dispose();
+    _inputs.dispose();
+
+    console.log(result, 'here')
+
+    const final = nnUtils.getMax(result[result.length-1])
+
+    console.log(result[result.length-1].indexOf(final),'lalal', result, final)
+    const word = [result[result.length-1].indexOf(final)]
+
+    return word;
+  }
+
   
 }
 
