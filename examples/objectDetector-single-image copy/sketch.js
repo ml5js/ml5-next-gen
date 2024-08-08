53c6b1f675b5563a06eb6c2c9e1792ef6c25d5a5
douaaz <94295853+douaaz@users.noreply.github.com>
2024-08-08 01:44:09 +0100

    add box for detected objects

--- examples/objectDetector-single-image copy/sketch.js
+++ examples/objectDetector-single-image copy/sketch.js
@@ -7,12 +7,12 @@
  */
 
 let objectDetector;
-let video;
-let hands = [];
+let img;
+let objects = [];
 
 function preload() {
   // Load the image to be detected
-  img = loadImage("family.jpeg");
+  img = loadImage("objects.jpg");
 
   // trying to work around "WebGPU readSync is only available for CPU-resident tensors."
   // see https://github.com/ml5js/ml5-next-gen/issues/117
@@ -26,17 +26,49 @@ function setup() {
   createCanvas(800, 800);
   // Draw the image
   image(img, 0, 0);
-  // Detect hands in an image
-  objectDetector.detect(img, gotHands);
+  // Detect objects in the image
+  objectDetector.detect(img, gotObjects);
 }
 
 function draw() {
-  // Draw all the hand keypoints
-  
+  // Draw the image
+  image(img, 0, 0);
+
+  // Loop through all the detected objects and draw bounding boxes with labels
+  for (let i = 0; i < objects.length; i++) {
+    let object = objects[i];
+    let x = object.bbox[0];
+    let y = object.bbox[1];
+    let w = object.bbox[2];
+    let h = object.bbox[3];
+
+    // Set the color
+    stroke(object.color.r, object.color.g, object.color.b);
+    noFill();
+
+    // Draw the bounding box
+    rect(x, y, w, h);
+
+    // Draw the label with the class name
+    noStroke();
+    fill(object.color.r, object.color.g, object.color.b);
+    textSize(16);
+    text(object.class, x + 5, y + 15);
+  }
 }
 
 // Callback function for when objectDetector outputs data
-function gotHands(results) {
-  // save the output to the hands variable
-  hands = results;
+function gotObjects(results) {
+  // Save the output to the objects variable and assign a random color to each object
+  objects = results.map(object => {
+    object.color = {
+      r: random(255),
+      g: random(255),
+      b: random(255)
+    };
+    return object;
+  });
+
+  // Redraw the canvas to update the bounding boxes
+  redraw();
 }
