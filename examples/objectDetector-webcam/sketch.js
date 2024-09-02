603514e508259ff4c6d9145e86e4fd829652ae60
douaaz <94295853+douaaz@users.noreply.github.com>
2024-09-02 09:27:30 +0800

    editing comments

--- examples/objectDetector-webcam/sketch.js
+++ examples/objectDetector-webcam/sketch.js
@@ -3,7 +3,7 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates object detection on an image through ml5.objectDetector.
+ * This example demonstrates object detection on a video through ml5.objectDetector.
  */
 
 let objectDetector;
