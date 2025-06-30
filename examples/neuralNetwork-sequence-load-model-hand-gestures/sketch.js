87989bfd41e5c50ef3cfcf96e52ab2e24ab90094
Mathew Ponon <mop9047@nyu.edu>
2025-06-30 11:58:30 -0400

    Fixed comment formatting

--- examples/neuralNetwork-sequence-load-model-hand-gestures/sketch.js
+++ examples/neuralNetwork-sequence-load-model-hand-gestures/sketch.js
@@ -24,7 +24,7 @@ function preload() {
   // Load the handPose model
   handPose = ml5.handPose();
 
-  // setup the timeseries neural network
+  // Setup the timeseries neural network
   let options = {
     task: "sequenceClassificationConv",
   };
@@ -36,53 +36,53 @@ function setup() {
   let canvas = createCanvas(640, 480);
   canvas.parent("canvasDiv");
 
-  // create video capture
+  // Create video capture
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.hide();
 
   handPose.detectStart(video, gotHands);
 
-  // setup the model files to load
+  // Setup the model files to load
   let modelDetails = {
     model: "model/model.json",
     metadata: "model/model_meta.json",
     weights: "model/model.weights.bin",
   };
 
-  // load the model and call modelLoaded once finished
+  // Load the model and call modelLoaded once finished
   model.load(modelDetails, modelLoaded);
 }
 
-// call back for load model
+// Callback for load model
 function modelLoaded() {
   console.log("model loaded!");
 }
 
 function draw() {
-  // draw video on the canvas
+  // Draw video on the canvas
   image(video, 0, 0, width, height);
 
-  // put the text on screen after a prediction
+  // Put the text on screen after a prediction
   placePredictedText();
 
-  // if hands are found then start recording
+  // If hands are found then start recording
   if (hands.length > 0) {
-    // get coordinates from hands (21 points)
+    // Get coordinates from hands (21 points)
     handpoints = drawPoints();
     sequence.push(handpoints);
 
-    // pad the data and use for prediction
+    // Pad the data and use for prediction
   } else if (hands.length <= 0 && sequence.length > 0) {
     let predictData = model.padCoordinates(sequence, targetLength);
     model.classify(predictData, gotResults);
 
-    // reset the sequence
+    // Reset the sequence
     sequence = [];
   }
 }
 
-// draw the points on the hands
+// Draw the points on the hands
 function drawPoints() {
   let handpoints = [];
   for (let i = 0; i < hands.length; i++) {
@@ -103,17 +103,17 @@ function drawPoints() {
 
 // Callback function for when handPose outputs data
 function gotHands(results) {
-  // save the output to the hands variable
+  // Save the output to the hands variable
   hands = results;
 }
 
-// call back for accessing the results
+// Call back for accessing the results
 function gotResults(results) {
   predictedWord = results[0].label;
   text(predictedWord, 100, 100);
 }
 
-// for drawing text on screen
+// For drawing text on screen
 function placePredictedText() {
   textSize(20);
   fill(0);
