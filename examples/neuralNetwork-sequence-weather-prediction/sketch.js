03c90e8512a91339f86c5b816fd728b83a790a04
gohai <gottfried.haider@gmail.com>
2025-07-15 16:03:24 +0800

    Tweaks to neuralNetwork-sequence-weather-prediction example

--- examples/neuralNetwork-sequence-weather-prediction/sketch.js
+++ examples/neuralNetwork-sequence-weather-prediction/sketch.js
@@ -3,16 +3,13 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates Loading JSON data and Training a Weather Predictor through ml5.neuralNetwork with sequeceRegression Task.
+ * This example demonstrates loading JSON data and training a Weather Predictor
+ * through ml5.neuralNetwork with the sequeceRegression task.
+ * XXX: let us know what this dataset is from/about
  */
 
 let model;
 let data;
-let graphValues = [];
-
-let state = "training";
-let precipitation = "";
-
 let features = [
   "temperature",
   "humidity",
@@ -21,127 +18,138 @@ let features = [
   "precipitation",
 ];
 
-let targets = features; // Must be the same to add predicted values back to data
-let windowLength = 10; // Optional: define the size of the window for batch
+let state = "training";
+let predictedRain = 0;
+let maxBars = 12;
+let graphValues = [];
 
-// Load JSON data with same formatting from the internet, this means
-// LoadData() cannot yet be used as it is formatted differently
 function preload() {
-  json_data = loadJSON("weather_data.json");
+  data = loadJSON("weather_data.json");
 
-  // Set the options to initialize Neural Network wit sequenceRegression Task
   let options = {
     task: "sequenceRegression",
-    debug: "true",
-    learningRate: 0.0075, // A smaller learning rate used for more stable training
+    debug: true,
+    learningRate: 0.0075, // smaller learning rate helps here
     inputs: features,
-    outputs: targets,
+    outputs: features,
   };
   model = ml5.neuralNetwork(options);
 }
 
 function setup() {
-  data = json_data.data;
-  createCanvas(640, 400);
-  background(220);
+  let canvas = createCanvas(640, 400);
+  canvas.parent("container");
 
-  // Run a sliding window algorithm for time based data
-  let batchData = model.slidingWindow(data, features, targets, windowLength);
+  // the JSON file has the actual data in a "data" property
+  data = data.data;
+
+  // XXX: the following is quite unclear
+  // XXX: - try naming the function with an action word (verb)
+  // XXX: - explain why we do this
+  // XXX: - the second and the third parameter need to be the same - just pass one then?
+  // XXX: - the last parameter is 10, but the result has 14 entries 🤷‍♂️ (data has 24)
+  // XXX: - we use the result for inputs and outputs, how about naming those
+  // XXX:   properties with those words directly (rather than "sequences", "targets")
+
+  // run a sliding window algorithm for time based data
+  let batchData = model.slidingWindow(data, features, features, 10);
   let inputs = batchData.sequences;
   let outputs = batchData.targets;
 
-  // Feed data into the model
+  // add the training data
   for (let i = 0; i < inputs.length; i++) {
     model.addData(inputs[i], outputs[i]);
   }
-  // Normalize the data after adding everything
   model.normalizeData();
 
+  // train right away
   let options = {
     epochs: 70,
   };
   model.train(options, finishedTraining);
 
-  UI();
+  let predictBtn = select("#predictBtn");
+  predictBtn.center();
+  predictBtn.mouseClicked(predictData);
 }
 
 function draw() {
   background(220);
+  noStroke();
   textSize(20);
-  if (state == "training") {
-    text("Training", 320, 200);
-  } else if (state == "prediction") {
-    text("Predicted Precipitation", 320, 200);
-    text(precipitation, 320, 250);
+  textAlign(CENTER);
 
-    // Helpful visual based on predicted value
+  if (state == "training") {
+    text("Training...", width / 2, 160);
+  } else if (state == "prediction") {
+    // XXX: let's add the unit here (mm? inches?)
+    text("Predicted rain: " + nf(predictedRain, 0, 1) + "", 320, 160);
     push();
-    textSize(precipitation * 5 + 10);
-    text("🌧️", 320, 150);
+    textSize(predictedRain * 5 + 10);
+    text("🌧️", width / 2, 100);
     pop();
   }
   drawBarGraph();
-}
-
-// Predict data
-function predictData() {
-  seq = model.sampleWindow(data); //Helper function paired with the slidingWindow to get sample from data
-  model.predict(seq, gotResults);
-}
-
-// Put the new data in the dataset so this will be considered for any new predictions
-function gotResults(results) {
-  precipitation = results[4].value;
-  addNewData(results); // Optional but will be helpful in using new prediction as part of dataset
-}
-
-// Code for adding new data to the dataset to be used for future prediction
-function addNewData(newResults) {
-  (new_values = {
-    date: " for the next hour",
-    temperature: newResults[0].value, // Get string convert to float and round to 2 decimal points
-    humidity: newResults[1].value,
-    wind_speed: newResults[2].value,
-    pressure: newResults[3].value,
-    precipitation: newResults[4].value,
-  }),
-    data.push(new_values);
-
-  // Add data to the bar graph
-  graphValues.push(newResults[4].value);
-  if (graphValues.length > maxBars) {
-    graphValues.shift(); // Remove first element
-  }
 }
 
 function finishedTraining() {
   state = "prediction";
 }
 
-// Get buttons and assign functions (UI)
-function UI() {
-  pred_but = select("#pred_but");
-  pred_but.mouseClicked(predictData);
+function predictData() {
+  // XXX: the following is similarly unclear
+  // XXX: - try naming the function with an action word (verb)
+  // XXX: - explain why we do this
+  // XXX: - (does this do more than return the last 10 samples from data?)
 
-  textAlign(CENTER);
+  // Helper function paired with the slidingWindow to get sample from data
+  let inputs = model.sampleWindow(data);
+  model.predict(inputs, gotResults);
+}
 
-  maxBars = 12;
-  barWidth = width / maxBars;
-  maxDataValue = 35;
+function gotResults(results) {
+  predictedRain = results[4].value;
+
+  // add result to the bar graph
+  graphValues.push(results[4].value);
+  if (graphValues.length > maxBars) {
+    graphValues.shift();
+  }
+
+  // optional: add predicted result to the dataset, so that it will be
+  // considered in further predictions (going forward in time)
+  addNewData(results);
+}
+
+function addNewData(newValues) {
+  data.push({
+    date: "One more (synthetic) hour",
+    temperature: newValues[0].value,
+    humidity: newValues[1].value,
+    wind_speed: newValues[2].value,
+    pressure: newValues[3].value,
+    precipitation: newValues[4].value,
+  });
 }
 
 function drawBarGraph() {
+  let barWidth = width / maxBars;
+  let maxDataValue = 35;
+
   for (let i = 0; i < graphValues.length && i < maxBars; i++) {
     let barHeight = map(graphValues[i], 0, maxDataValue, 0, height - 180);
     let x = i * barWidth;
     let y = height - barHeight - 20;
 
-    // Bar color gradient based on value
+    // XXX: the colors are a bit unintuitive (blue is normally associated
+    // with more rain rather than less) - maybe you could do some research
+    // and use lerpColor() here?
+
     let barColor = map(graphValues[i], 0, maxDataValue, 0, 255);
+    push();
     fill(barColor, 100, 255 - barColor);
     stroke(100);
-
-    // Draw bar
     rect(x + 5, y, barWidth - 10, barHeight);
+    pop();
   }
 }
