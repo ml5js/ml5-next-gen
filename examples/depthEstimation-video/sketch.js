5f0e3fb177962b557507baaf6af7d51f6ecea735
nasif-co <nasifrincon96@gmail.com>
2025-07-29 21:48:03 -0400

    Make HTML video element example
    Since we already have a webcam video example, it felt redundant to have the depthEstimation-video example also use the webcam. So I modified it to instead showcase how to run depthEstimation with a video file.

--- examples/depthEstimation-video/sketch.js
+++ examples/depthEstimation-video/sketch.js
@@ -3,7 +3,7 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates basic depth estimation on a video feed using ml5.depthEstimation.
+ * This example demonstrates basic depth estimation on an mp4 video using ml5.depthEstimation.
  */
 
 let depthEstimator;
@@ -14,36 +14,42 @@ let depthMap;
 let videoWidth = 640;
 let videoHeight = 480;
 
-// Default options for depth estimation (can be adjusted if needed)
-
 function preload() {
   // Initialize the depth estimation model
   depthEstimator = ml5.depthEstimation();
 }
 
 function setup() {
-  // Create a canvas twice the width of the video
-  createCanvas(videoWidth * 2, videoHeight);
+  // Create a canvas the size of the video
+  createCanvas(videoWidth, videoHeight);
 
-  // Create the video capture element
-  video = createCapture(VIDEO);
+  // Create the video
+  video = createVideo("presenter.mp4"); // Excerpt from "Your name here", a 1960's spoof video in the public domain
+  video.volume(0); // Mute is required for autoplay in some browsers
+  video.autoplay(true); // Start playing the video automatically
+
+  // When the video loads, run the 'videoLoaded' function
+  video.elt.addEventListener("loadeddata", videoLoaded);
+
   video.size(videoWidth, videoHeight); // Set video size
-  video.hide(); // Hide the default HTML video element
 
-  // Start continuous depth estimation on the video feed
+  // Uncomment to hide the video element
+  //video.hide();
+}
+
+// When loaded, start continuous depth estimation on the video feed
+function videoLoaded() {
+  video.loop();
   depthEstimator.estimateStart(video, gotResults);
 }
 
 function draw() {
   background(0);
 
-  // Draw the original video feed on the left half of the canvas
-  image(video, 0, 0);
-
   // Check if depth estimation results are available
-  if (depthMap ) {
-    // Draw the colormapped depth visualization on the right half of the canvas
-    image(depthMap.image, videoWidth, 0, videoWidth, videoHeight);
+  if (depthMap) {
+    // Draw the depth map of the video
+    image(depthMap.image, 0, 0);
   }
 }
 
