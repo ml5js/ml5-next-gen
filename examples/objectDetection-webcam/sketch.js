ff35a3c22022f01b679dcecbff0bb54720d6249b
Bairui Su <subairui@icloud.com>
2025-10-08 15:21:45 -0400

    Update example to use ml5.objectDetector

--- examples/objectDetection-webcam/sketch.js
+++ examples/objectDetection-webcam/sketch.js
@@ -3,7 +3,7 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates detecting objects in a live video through ml5.imageClassifier.
+ * This example demonstrates detecting objects in a live video through ml5.objectDetector.
  */
 
 let video;
