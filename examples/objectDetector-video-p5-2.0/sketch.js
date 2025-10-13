9b6f5d252d59c5c988bb5a1ec08f54fd7ce5e669
Jin <40701844+yiyujin@users.noreply.github.com>
2025-10-13 15:26:09 -0400

    Object detector add p5 2.0 examples (#276)
    * rename example to objectDetector not objectDetection and copy pasted 1.0 examples for 2.0 examples
    
    * add p5 2.0 examples, fix typo

---
+++ examples/objectDetector-video-p5-2.0/sketch.js
@@ -0,0 +1,53 @@
+/*
+* 👋 Hello! This is an ml5.js example made and shared with ❤️.
+* Learn more about the ml5.js project: https://ml5js.org
+* ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+*
+* This example demonstrates detecting objects in a video file through ml5.objectDetector.
+*/
+
+let video;
+let detector;
+let detections = [];
+
+// Callback function is called each time the object detector finishes processing a frame.
+function gotDetections(results) {
+  // Update detections array with the new results
+  detections = results;  
+}
+
+async function setup() {
+  detector = await ml5.objectDetector("cocossd");
+  
+  createCanvas(640, 480);
+  
+  // Load and loop the video for object detection
+  video = createVideo('ball_lifting.mp4'); // video sized 640 x 480
+  video.hide();
+  video.loop();
+
+  detector.detectStart(video, gotDetections);
+}
+
+function draw(){
+  image(video, 0, 0); // draw video frame
+
+  for (let i = 0; i < detections.length; i++) {
+    let detection = detections[i];
+
+    let x = detection.x;
+    let y = detection.y;
+    let w = detection.width;
+    let h = detection.height;
+
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(x, y, w, h);
+
+    noStroke();
+    fill(255);
+    textSize(18);
+    text(detection.label, x + 5, y + 20);
+  }
+}
---
+++ examples/objectDetector-webcam-p5-2.0/sketch.js
@@ -0,0 +1,51 @@
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates detecting objects in a live video through ml5.objectDetector.
+ */
+
+let video;
+let detector;
+let detections = [];
+
+async function setup() {
+  detector = await ml5.objectDetector("cocossd");
+
+  createCanvas(640, 480);
+
+  // Using webcam feed as video input, hiding html element to avoid duplicate with canvas
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  detector.detectStart(video, gotDetections);
+}
+
+// Callback function is called each time the object detector finishes processing a frame.
+function gotDetections(results) {
+  // Update detections array with the new results
+  detections = results;
+}
+
+function draw() {
+  // Draw the current video frame onto the canvas.
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i += 1) {
+    let detection = detections[i];
+
+    // Draw bounding box
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    noFill();
+    rect(detection.x, detection.y, detection.width, detection.height);
+
+    // Draw label
+    noStroke();
+    fill(255);
+    textSize(24);
+    text(detection.label, detection.x + 10, detection.y + 24);
+  }
+}
