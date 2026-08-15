decbe71fd7d415f8a764fdd6ab6a80928edd595d
Daniel Shiffman <daniel.shiffman@gmail.com>
2026-08-15 10:19:30 -0400

    Update examples/imageClassifier-single-image/sketch.js

--- examples/imageClassifier-single-image/sketch.js
+++ examples/imageClassifier-single-image/sketch.js
@@ -3,8 +3,8 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates detecting objects in an image through ml5.imageClassifier.
- * Try this example with the transformer model, "ViTBase", it's trained on the same 1000 images as MobileNet
+* This example demonstrates image classification with ml5.imageClassifier.
+* Try this example with the transformer model "ViTBase", which is trained to recognize the same 1,000 ImageNet labels as MobileNet.
  */
 
 // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
